bce5506284d738600068ec8233319acd38f41376
Tanti Wulansari <tantiwulansari2603@gmail.com>
2023-11-01 11:00:39 +0700

    Dashboard

--- absensi-app-main/app/Http/Controllers/DashboardController.php
+++ absensi-app-main/app/Http/Controllers/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Position;
 use App\Models\User;
+use App\Models\Presence;
+use App\Models\Attendance;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,7 +15,9 @@ class DashboardController extends Controller
         return view('dashboard.index', [
             "title" => "Dashboard",
             "positionCount" => Position::count(),
-            "userCount" => User::count()
+            "userCount" => User::count(),
+            "presenceCount" => Presence::count(),
+            "attendanceCount" => Attendance::count(),
         ]);
     }
 }
