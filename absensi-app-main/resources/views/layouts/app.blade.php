10a5c67c52455cce6d4f1b50a03d591d97e7af85
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2023-11-15 08:28:30 +0700

    edit logo navbar

--- absensi-app-main/resources/views/layouts/app.blade.php
+++ absensi-app-main/resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <div class="page-main-header">
         <div class="main-header-right row m-0">
             <div class="main-header-left">
-                <div class="logo-wrapper"><a href="{{ route('home.index') }}"><img class="img-fluid" src="{{ asset('assets/images/logonatusi.png') }}" alt=""></a></div>
-                <div class="dark-logo-wrapper"><a href="{{ route('home.index') }}"><img class="img-fluid" src="{{ asset('assets/images/logonatusi.png') }}" alt=""></a></div>
+                <div class="logo-wrapper"><a href="{{ route('home.index') }}"><img class="img" src="{{ asset('assets/images/logonatusi.png') }}" alt="" height="50px"></a></div>
+                <div class="dark-logo-wrapper"><a href="{{ route('home.index') }}"><img class="img" src="{{ asset('assets/images/logonatusi.png') }}" alt="" height="50px"></a></div>
                 <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle"></i></div>
             </div>
             <!-- Page Header Start-->
