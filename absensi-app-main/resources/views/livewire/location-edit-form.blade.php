50339b8eb7c914e1b129f52526d0385b691a35d4
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2023-12-13 11:46:01 +0700

    update edit form locations

--- absensi-app-main/resources/views/livewire/location-edit-form.blade.php
+++ absensi-app-main/resources/views/livewire/location-edit-form.blade.php
@@ -5,35 +5,31 @@
         <div class="mb-3">
             <div class="w-100">
                 <div class="mb-3">
-                    <x-form-label id="nama{{ $i }}" label='Nama Kantor ({{ $i + 1 }})' />
-                    <x-form-input id="nama{{ $i }}" name="nama{{ $i }}"
-                        wire:model.defer="locations.{{ $i }}.nama" />
-                    <x-form-error key="locations.{{ $i }}.nama" />
+                    <x-form-label id="nama" label='Nama Kantor' />
+                    <x-form-input id="nama" name="nama" wire:model.defer="locations.0.nama" />
+                    <x-form-error key="locations.0.nama" />
                 </div>
                 <div class="mb-3">
-                    <x-form-label id="alamat{{ $i }}" label='Alamat ({{ $i + 1 }})' />
-                    <textarea id="alamat{{ $i }}" name="alamat{{ $i }}" class="form-control"
-                        wire:model.defer="locations.{{ $i }}.alamat"></textarea>
-                    <x-form-error key="locations.{{ $i }}.alamat" />
+                    <x-form-label id="alamat" label='Alamat' />
+                    <textarea id="alamat" name="alamat" class="form-control" wire:model.defer="locations.0.alamat"></textarea>
+                    <x-form-error key="locations.0.alamat" />
                 </div>
                 <div class="mb-3">
-                    <x-form-label id="location{{ $i }}" label='Pilih Lokasi ({{ $i + 1 }})' />
-                    <div id="map{{ $i }}" style="height: 300px;"></div>
+                    <x-form-label id="location" label='Pilih Lokasi' />
+                    <div id="map" style="height: 300px;"></div>
                     <div class="row">
                         <div class="col-md-6">
                             <!-- Latitude -->
-                            <x-form-label id="latitude{{ $i }}" label='Latitude ({{ $i + 1 }})' />
-                            <x-form-input id="latitude{{ $i }}" name="latitude{{ $i }}" type="text"
-                                wire:model.defer="locations.{{ $i }}.latitude" />
-                            <x-form-error key="locations.{{ $i }}.latitude" />
+                            <x-form-label id="latitude" label='Latitude' />
+                            <x-form-input id="latitude" name="latitude" type="text" wire:model.defer="locations.0.latitude" />
+                            <x-form-error key="locations.0.latitude" />
                         </div>
-                
+
                         <div class="col-md-6">
                             <!-- Longitude -->
-                            <x-form-label id="longitude{{ $i }}" label='Longitude ({{ $i + 1 }})' />
-                            <x-form-input id="longitude{{ $i }}" name="longitude{{ $i }}" type="text"
-                                wire:model.defer="locations.{{ $i }}.longitude" />
-                            <x-form-error key="locations.{{ $i }}.longitude" />
+                            <x-form-label id="longitude" label='Longitude' />
+                            <x-form-input id="longitude" name="longitude" type="text" wire:model.defer="locations.0.longitude" />
+                            <x-form-error key="locations.0.longitude" />
                         </div>
                     </div>
                 </div>
@@ -48,4 +44,46 @@
             </button>
         </div>
     </form>
+    <script>
+        document.addEventListener('livewire:load', function() {
+            var initialLatitude = @json($locations[0]['latitude']);
+            var initialLongitude = @json($locations[0]['longitude']);
+
+            var map = L.map('map').setView([initialLatitude, initialLongitude], 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+            var marker = L.marker([initialLatitude, initialLongitude], {
+                draggable: true
+            }).addTo(map);
+
+            marker.on('dragend', function(event) {
+                var lat = event.target.getLatLng().lat;
+                var lng = event.target.getLatLng().lng;
+                document.getElementById('latitude').value = lat;
+                document.getElementById('longitude').value = lng;
+
+                // Koordinat marker
+                var markerCoordinates = [lat, lng];
+
+                // Hapus semua marker sebelumnya
+                map.eachLayer(function(layer) {
+                    if (layer instanceof L.CircleMarker) {
+                        layer.remove();
+                    }
+                });
+
+                // Buat marker lingkaran dengan warna hijau dan radius 10 meter
+                var circleMarker = L.circle(markerCoordinates, {
+                    color: 'green',
+                    fillColor: 'green',
+                    fillOpacity: 0.5,
+                    radius: 10
+                }).addTo(map);
+
+                // Tambahkan popup jika diperlukan
+                circleMarker.bindPopup('Ini adalah lingkaran marker dengan radius 10 meter').openPopup();
+            });
+        });
+    </script>
 </div>
