041c0ac4ee06e966ec1be91f98e8590e2b058fba
Firdaus <firmanzaki21@gmail.com>
2023-12-13 11:26:52 +0700

    update_location_create_map

--- absensi-app-main/resources/views/livewire/location-create-form.blade.php
+++ absensi-app-main/resources/views/livewire/location-create-form.blade.php
@@ -1,93 +1,87 @@
 <div>
     <form wire:submit.prevent="saveLocations" method="post" novalidate>
         @include('partials.alerts')
-        @foreach ($locations as $i => $location)
-            <div class="mb-3">
-                <div class="w-100">
-                    <div class="mb-3">
-                        <x-form-label id="nama{{ $i }}" label='Nama Kantor ({{ $i + 1 }})' />
-                        <x-form-input id="nama{{ $i }}" name="nama{{ $i }}"
-                            wire:model.defer="locations.{{ $i }}.nama" />
-                        <x-form-error key="locations.{{ $i }}.nama" />
-                    </div>
-                    <div class="mb-3">
-                        <x-form-label id="alamat{{ $i }}" label='Alamat ({{ $i + 1 }})' />
-                        <textarea id="alamat{{ $i }}" name="alamat{{ $i }}" class="form-control"
-                            wire:model.defer="locations.{{ $i }}.alamat"></textarea>
-                        <x-form-error key="locations.{{ $i }}.alamat" />
-                    </div>
-                    <div class="mb-3">
-                        <x-form-label id="location{{ $i }}" label='Pilih Lokasi ({{ $i + 1 }})' />
-                        <div id="map{{ $i }}" style="height: 300px;"></div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <!-- Latitude -->
-                                <x-form-label id="latitude{{ $i }}" label='Latitude ({{ $i + 1 }})' />
-                                <x-form-input id="latitude{{ $i }}" name="latitude{{ $i }}" type="text"
-                                    wire:model.defer="locations.{{ $i }}.latitude" />
-                                <x-form-error key="locations.{{ $i }}.latitude" />
-                            </div>
-                    
-                            <div class="col-md-6">
-                                <!-- Longitude -->
-                                <x-form-label id="longitude{{ $i }}" label='Longitude ({{ $i + 1 }})' />
-                                <x-form-input id="longitude{{ $i }}" name="longitude{{ $i }}" type="text"
-                                    wire:model.defer="locations.{{ $i }}.longitude" />
-                                <x-form-error key="locations.{{ $i }}.longitude" />
-                            </div>
+
+        <div class="mb-3">
+            <div class="w-100">
+                <div class="mb-3">
+                    <x-form-label id="nama" label='Nama Kantor' />
+                    <x-form-input id="nama" name="nama" wire:model.defer="locations.0.nama" />
+                    <x-form-error key="locations.0.nama" />
+                </div>
+                <div class="mb-3">
+                    <x-form-label id="alamat" label='Alamat' />
+                    <textarea id="alamat" name="alamat" class="form-control" wire:model.defer="locations.0.alamat"></textarea>
+                    <x-form-error key="locations.0.alamat" />
+                </div>
+                <div class="mb-3">
+                    <x-form-label id="location" label='Pilih Lokasi' />
+                    <div id="map" style="height: 500px;"></div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <!-- Latitude -->
+                            <x-form-label id="latitude" label='Latitude' />
+                            <x-form-input id="latitude" name="latitude" type="text"
+                                wire:model.defer="locations.0.latitude" />
+                            <x-form-error key="locations.0.latitude" />
+                        </div>
+
+                        <div class="col-md-6">
+                            <!-- Longitude -->
+                            <x-form-label id="longitude" label='Longitude' />
+                            <x-form-input id="longitude" name="longitude" type="text"
+                                wire:model.defer="locations.0.longitude" />
+                            <x-form-error key="locations.0.longitude" />
                         </div>
                     </div>
                 </div>
-                @if ($i > 0)
-                    <button class="btn btn-sm btn-danger mt-2" wire:click="removeLocationInput({{ $i }})"
-                        wire:target="removeLocationInput({{ $i }})" type="button"
-                        wire:loading.attr="disabled">Hapus</button>
-                @endif
             </div>
-            <hr>
-        @endforeach
+        </div>
+        <hr>
 
         <div class="d-flex justify-content-between align-items-center mb-5">
             <button class="btn btn-primary">
                 Simpan
             </button>
-            <button class="btn btn-light" type="button" wire:click="addLocationInput" wire:loading.attr="disabled">
-                Tambah Input
-            </button>
         </div>
     </form>
-    @push('scripts')
-        @foreach ($locations as $i => $location)
-            <script>
-                var map{{ $i }} = L.map('map{{ $i }}').setView([-7.488679253144124, 112.46195241611329], 13);
+    <script>
+        document.addEventListener('livewire:load', function () {
+            var map = L.map('map').setView([-7.488679253144124, 112.46195241611329], 13);
 
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map{{ $i }});
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-                var marker{{ $i }} = L.marker([-7.488679253144124, 112.46195241611329], {
-                    draggable: true
-                }).addTo(map{{ $i }});
+            var marker = L.marker([-7.488679253144124, 112.46195241611329], {
+                draggable: true
+            }).addTo(map);
 
-                marker{{ $i }}.on('dragend', function(event) {
-                    var lat = event.target.getLatLng().lat;
-                    var lng = event.target.getLatLng().lng;
-                    document.getElementById('latitude{{ $i }}').value = lat;
-                    document.getElementById('longitude{{ $i }}').value = lng;
+            marker.on('dragend', function (event) {
+                var lat = event.target.getLatLng().lat;
+                var lng = event.target.getLatLng().lng;
+                document.getElementById('latitude').value = lat;
+                document.getElementById('longitude').value = lng;
 
-                    // Koordinat marker
-                    var markerCoordinates = [-7.488679253144124, 112.46195241611329];
+                // Koordinat marker
+                var markerCoordinates = [lat, lng];
 
-                    // Buat marker lingkaran dengan warna hijau dan radius 10 meter
-                    var circleMarker = L.circle(markerCoordinates, {
-                        color: 'green',
-                        fillColor: 'green',
-                        fillOpacity: 0.5,
-                        radius: 10
-                    }).addTo(map);
-
-                    // Tambahkan popup jika diperlukan
-                    circleMarker.bindPopup('Ini adalah lingkaran marker dengan radius 10 meter').openPopup();
+                // Hapus semua marker sebelumnya
+                map.eachLayer(function (layer) {
+                    if (layer instanceof L.CircleMarker) {
+                        layer.remove();
+                    }
                 });
-            </script>
-        @endforeach
-    @endpush
+
+                // Buat marker lingkaran dengan warna hijau dan radius 10 meter
+                var circleMarker = L.circle(markerCoordinates, {
+                    color: 'green',
+                    fillColor: 'green',
+                    fillOpacity: 0.5,
+                    radius: 10
+                }).addTo(map);
+
+                // Tambahkan popup jika diperlukan
+                circleMarker.bindPopup('Ini adalah lingkaran marker dengan radius 10 meter').openPopup();
+            });
+        });
+    </script>
 </div>
