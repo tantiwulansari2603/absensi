7985addef809a734794d89bd5e0ed8abc3fb616d
Firdaus <firmanzaki21@gmail.com>
2023-12-15 08:56:53 +0700

    update_create_lokasi

--- absensi-app-main/resources/views/livewire/location-create-form.blade.php
+++ absensi-app-main/resources/views/livewire/location-create-form.blade.php
@@ -1,7 +1,6 @@
 <div>
     <form wire:submit.prevent="saveLocations" method="post" novalidate>
         @include('partials.alerts')
-
         <div class="mb-3">
             <div class="w-100">
                 <div class="mb-3">
@@ -16,12 +15,12 @@
                 </div>
                 <div class="mb-3">
                     <x-form-label id="location" label='Pilih Lokasi' />
-                    <div id="map" style="height: 500px;"></div>
+                    <div id="map" style="height: 500px;" wire:ignore></div>
                     <div class="row">
                         <div class="col-md-6">
                             <!-- Latitude -->
                             <x-form-label id="latitude" label='Latitude' />
-                            <x-form-input id="latitude" name="latitude" type="text"
+                            <x-form-input id="latitude" name="latitude" type="text" readonly
                                 wire:model.defer="locations.0.latitude" />
                             <x-form-error key="locations.0.latitude" />
                         </div>
@@ -29,7 +28,7 @@
                         <div class="col-md-6">
                             <!-- Longitude -->
                             <x-form-label id="longitude" label='Longitude' />
-                            <x-form-input id="longitude" name="longitude" type="text"
+                            <x-form-input id="longitude" name="longitude" type="text" readonly
                                 wire:model.defer="locations.0.longitude" />
                             <x-form-error key="locations.0.longitude" />
                         </div>
@@ -46,8 +45,8 @@
         </div>
     </form>
     <script>
-        document.addEventListener('livewire:load', function () {
-            var map = L.map('map').setView([-7.488679253144124, 112.46195241611329], 13);
+        document.addEventListener('livewire:load', function() {
+            var map = L.map('map').setView([-7.488679253144124, 112.46195241611329], 15);
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
@@ -55,32 +54,52 @@
                 draggable: true
             }).addTo(map);
 
-            marker.on('dragend', function (event) {
-                var lat = event.target.getLatLng().lat;
-                var lng = event.target.getLatLng().lng;
-                document.getElementById('latitude').value = lat;
-                document.getElementById('longitude').value = lng;
-
-                // Koordinat marker
-                var markerCoordinates = [lat, lng];
+            function updateMarker(lat, lng) {
+                marker.setLatLng([lat, lng]);
 
                 // Hapus semua marker sebelumnya
-                map.eachLayer(function (layer) {
+                map.eachLayer(function(layer) {
                     if (layer instanceof L.CircleMarker) {
                         layer.remove();
                     }
                 });
 
                 // Buat marker lingkaran dengan warna hijau dan radius 10 meter
-                var circleMarker = L.circle(markerCoordinates, {
+                var circleMarker = L.circle([lat, lng], {
                     color: 'green',
                     fillColor: 'green',
                     fillOpacity: 0.5,
                     radius: 10
                 }).addTo(map);
 
-                // Tambahkan popup jika diperlukan
-                circleMarker.bindPopup('Ini adalah lingkaran marker dengan radius 10 meter').openPopup();
+                // Tambahkan event hover
+                circleMarker.on('mouseover', function(event) {
+                    this.bindPopup('Area lokasi dengan radius 10 meter').openPopup();
+                });
+
+                circleMarker.on('mouseout', function(event) {
+                    this.closePopup();
+                });
+
+                // Update nilai latitude dan longitude pada form Livewire
+                @this.set('locations.0.latitude', lat);
+                @this.set('locations.0.longitude', lng);
+            }
+
+            map.on('click', function(e) {
+                var lat = e.latlng.lat;
+                var lng = e.latlng.lng;
+
+                // Panggil fungsi updateMarker dengan koordinat baru
+                updateMarker(lat, lng);
+            });
+
+            marker.on('dragend', function(event) {
+                var lat = event.target.getLatLng().lat;
+                var lng = event.target.getLatLng().lng;
+
+                // Panggil fungsi updateMarker dengan koordinat baru
+                updateMarker(lat, lng);
             });
         });
     </script>
