817139e230ca59e1b6b2cbdf02aaf19cb6b7ebe6
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2023-12-20 09:13:38 +0700

    edit navbar user dan dosen

--- absensi-app-main/resources/views/partials/home-navbar.blade.php
+++ absensi-app-main/resources/views/partials/home-navbar.blade.php
@@ -6,9 +6,24 @@
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav align-items-md-center gap-md-4 py-2 py-md-0">
+                @php
+                $userRole = auth()->user()->role_id ?? null; // Mendapatkan peran pengguna (role), jika ada
+                @endphp
+                <!-- Navbar Button -->
+                @if($userRole === 3)
                 <li class="nav-item px-4 py-1 px-md-0 py-md-0">
-                    <a class="nav-link {{ request()->routeIs('home.*') ? 'active fw-bold' : '' }}" aria-current="page" href="{{ route('home.index') }}" style="color: white;">Dashboard</a>
+                    <a class="nav-link {{ request()->routeIs('dosen.*') ? 'active fw-bold' : '' }}" aria-current="page" href="{{ route('dosen.index') }}" style="color: white;">
+                        Dashboard
+                    </a>
                 </li>
+                @endif
+                @if($userRole === 2)
+                <li class="nav-item px-4 py-1 px-md-0 py-md-0">
+                    <a class="nav-link {{ request()->routeIs('home.*') ? 'active fw-bold' : '' }}" aria-current="page" href="{{ route('home.index') }}" style="color: white;">
+                        Dashboard
+                    </a>
+                </li>
+                @endif
                 <li class="nav-item px-4 py-1 px-md-0 py-md-0">
                     <form action="{{ route('auth.logout') }}" method="post" onsubmit="return confirm('Apakah anda yakin ingin keluar?')">
                         @method('DELETE')
